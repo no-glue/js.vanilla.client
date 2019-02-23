f994be2cc4d9eb6fd26ba771f1f25110d076feed
nikolapav1985 <nikolapav1985@gmail.com>
2019-02-23 19:57:15 +0100

    signup

--- client/js/custom.js
+++ client/js/custom.js
@@ -11,3 +11,20 @@ var makeListEmpty=function(list){
         list.removeChild(list.lastChild);
     }
 }
+
+/**
+* addClassToElements
+*
+* add some class to elements
+*
+* items - these have class added
+*
+* clss - class to add
+*
+**/
+var addClassToElements = function(list,clss){
+    var i=0;
+    for(;i<list.length;i++){
+        list[i].classList.add(clss);
+    }
+}
