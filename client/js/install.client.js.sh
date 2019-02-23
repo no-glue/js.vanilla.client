261c713946feeb4a1e1694928a802dcf3160e760
nikolapav1985 <nikolapav1985@gmail.com>
2019-02-23 17:11:38 +0100

    track

---
+++ client/js/install.client.js.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+wget https://raw.githubusercontent.com/jgallen23/routie/master/dist/routie.min.js
+#make the file executable eg sudo chmod 755 install.client.js.sh
+#execute file ./install.client.js.sh
