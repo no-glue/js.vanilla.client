ab5a9100283cbce291d8c6aad261155954d53264
nikolapav1985 <nikolapav1985@gmail.com>
2019-02-25 22:43:29 +0100

    class remove

--- client/js/install.client.js.sh
+++ client/js/install.client.js.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 wget https://raw.githubusercontent.com/jgallen23/routie/master/dist/routie.min.js
 wget https://raw.githubusercontent.com/blueimp/JavaScript-MD5/master/js/md5.js
+wget https://raw.githubusercontent.com/alexei/sprintf.js/master/src/sprintf.js
 #make the file executable eg sudo chmod 755 install.client.js.sh
 #execute file ./install.client.js.sh
